95706b97ada4209b2bd0075232c7154e06380b34
Quinn Dumala <quinndumala@gmail.com>
2025-06-15 10:39:24 +0800

    Remove pages folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
 "use client";
-import HomePage from "./pages/HomePage";
+import React, { useEffect } from "react";
+import Grid from "./components/Grid";
+import PokemonList from "./components/PokemonList";
+import useGetInfinitePokemon from "./hooks/useGetInfinitePokemon";
+import Loading from "./components/Loading";
 
-export default function Page() {
-  return <HomePage />;
+export default function HomePage() {
+  const { ref, pokemons, error, isFetchingNextPage, status } =
+    useGetInfinitePokemon();
+
+  useEffect(() => {
+    console.log("pokemons...", pokemons);
+  }, [pokemons]);
+
+  if (status === "error" && error) return <p>Error: {error.message}</p>;
+
+  return (
+    <>
+      <Grid>
+        <PokemonList pokemons={pokemons} innerRef={ref} />
+      </Grid>
+      <div>{isFetchingNextPage && <Loading />}</div>
+    </>
+  );
 }
