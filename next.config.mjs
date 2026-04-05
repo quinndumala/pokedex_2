415159d9e041d8520bdd41bba07625603589a65f
Quinn Dumala <quinndumala@gmail.com>
2026-04-05 11:46:55 +0800

    Add new image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images : {
-        domains : [
-            "localhost",
-            "assets.pokemon.com",
-            "images.pokemontcg.io",
-            "raw.githubusercontent.com",
-        ]
-      }
+  images: {
+    domains: [
+      "localhost",
+      "assets.pokemon.com",
+      "images.pokemontcg.io",
+      "raw.githubusercontent.com",
+      "images.scrydex.com",
+    ],
+  },
 };
 
 export default nextConfig;
